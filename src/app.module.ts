0d5cb79dd03d94d81f2e19af438aac23536d7bd9
TazmanS <TazmanS123@gmail.com>
2024-07-18 17:43:48 +0300

    connect env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TodoModule } from './todo/todo.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      'mongodb+srv://tazmans123:RnUMOk0MPqNKgNQD@todo.rzan1o1.mongodb.net/?retryWrites=true&w=majority&appName=todo',
+      `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@todo.rzan1o1.mongodb.net/`,
     ),
     TodoModule,
   ],
